6c488af4d15421396bd428db4adcf15d4950c7f3
bcollet <bcollet@mathworks.com>
2022-12-05 11:09:32 +0100

    Add day 1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# advent-of-code-2022
+# [Advent of Code 2022](https://adventofcode.com/2022)
+
 Advent of Code is an Advent calendar of small programming puzzles for a variety of skill sets and skill levels that can be solved in any programming language you like.
---
+++ day1_calorie_counting.py
@@ -0,0 +1,35 @@
+def main(the_list: str) -> None:
+    calories = [0]
+    for item in the_list.splitlines():
+        try:
+            calories[-1] += int(item)
+        except ValueError:
+            calories.append(0)
+
+    calories.sort(reverse=True)
+
+    print("Part One: ")
+    print(calories[0])
+
+    print("Part Two:")
+    print(sum(calories[:3]))
+
+
+if __name__ == "__main__":
+    INPUT = """1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+"""
+
+    main(INPUT)
